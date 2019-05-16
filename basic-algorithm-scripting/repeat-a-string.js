9bc8268babd19f9c02a21b45b50917d20d2c5678
Ryan Silva <RyandotSilva@gmail.com>
2019-05-15 21:28:09 -0400

    Add solution

--- basic-algorithm-scripting/repeat-a-string.js
+++ basic-algorithm-scripting/repeat-a-string.js
@@ -9,4 +9,11 @@ function repeatStringNumTimes(str, num) {
 	return repeatedString;
 }
 
+// Solve using recursion
+function repeatStringNumTimes(str, num) {
+	if (num < 0) return '';
+	if (num === 1) return str;
+	else return str + repeatStringNumTimes(str, num - 1);
+}
+
 repeatStringNumTimes('abc', 3);
