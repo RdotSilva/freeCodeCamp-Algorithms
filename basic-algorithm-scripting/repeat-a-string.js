84106ea6e8e6fa44d75ffc80829c450fe6c4953f
Ryan Silva <RyandotSilva@gmail.com>
2019-09-23 19:31:58 -0400

    Update solution

--- basic-algorithm-scripting/repeat-a-string.js
+++ basic-algorithm-scripting/repeat-a-string.js
@@ -1,24 +1,13 @@
 // Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
 
-function repeatStringNumTimes(str, num) {
-	let repeatedString = '';
-	while (num > 0) {
+const repeatStringNumTimes = (str, num) => {
+	let repeatedString = "";
+
+	for (let i = 0; i < num; i++) {
 		repeatedString += str;
-		num--;
 	}
+
 	return repeatedString;
-}
+};
 
-// Solve using recursion
-function repeatStringNumTimes(str, num) {
-	if (num < 0) return '';
-	if (num === 1) return str;
-	else return str + repeatStringNumTimes(str, num - 1);
-}
-
-// Declarative solution
-function repeatStringNumTimes(str, num) {
-	return num > 0 ? str.repeat(num) : '';
-}
-
-repeatStringNumTimes('abc', 3);
+console.log(repeatStringNumTimes("abc", -1));
