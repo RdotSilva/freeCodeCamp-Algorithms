f8dc11fa76c6edef5dc2a6a9aec1f6f002bae06f
Ryan Silva <RyandotSilva@gmail.com>
2019-09-23 18:45:52 -0400

    Update solution

--- basic-algorithm-scripting/find-the-longest-word-in-a-string.js
+++ basic-algorithm-scripting/find-the-longest-word-in-a-string.js
@@ -2,17 +2,31 @@
 
 // Your response should be a number.
 
-function findLongestWordLength(str) {
-	let arrayOfStrings = str.split(' ');
-	let longestStringLength = 0;
-	let longestString = '';
-	for (let i = 0; i < arrayOfStrings.length; i++) {
-		if (arrayOfStrings[i].length > longestStringLength) {
-			longestStringLength = arrayOfStrings[i].length;
-			longestString = arrayOfStrings[i];
-		}
-	}
-	return longestString.length;
-}
+// function findLongestWordLength(str) {
+// 	let arrayOfStrings = str.split(" ");
+// 	let longestStringLength = 0;
+// 	for (let i = 0; i < arrayOfStrings.length; i++) {
+// 		if (arrayOfStrings[i].length > longestStringLength) {
+// 			longestStringLength = arrayOfStrings[i].length;
+// 			longestString = arrayOfStrings[i];
+// 		}
+// 	}
+// 	return longestString.length;
+// }
 
-findLongestWordLength('The quick brown fox jumped over the lazy dog');
+const findLongestWordLength = str => {
+	let wordsSplit = str.split(" ");
+	let longestWordLength = 0;
+
+	// Check each word in the array for length & update longest length variable.
+	wordsSplit.forEach(element => {
+		if (element.length > longestWordLength) {
+			longestWordLength = element.length;
+		}
+	});
+	return longestWordLength;
+};
+
+console.log(
+	findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
