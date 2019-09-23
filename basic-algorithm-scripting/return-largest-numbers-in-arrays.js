a9b51f4a8b7e1903ff2f438909dcf117f888ef5e
Ryan Silva <RyandotSilva@gmail.com>
2019-09-23 19:13:54 -0400

    Update solution

--- basic-algorithm-scripting/return-largest-numbers-in-arrays.js
+++ basic-algorithm-scripting/return-largest-numbers-in-arrays.js
@@ -2,29 +2,30 @@
 
 // Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
-function largestOfFour(arr) {
-	// Create final array to return output
-	let finalArray = [];
+const largestOfFour = arr => {
+	let largestArray = [];
+	let counter;
 
+	// Loop for initial array.
 	for (let i = 0; i < arr.length; i++) {
-		// Init largest number as first item in array
-		let largestNumber = arr[i][0];
-
-		for (let x = 1; x < arr[i].length; x++) {
-			if (arr[i][x] > largestNumber) {
-				// Loop through numbers in array and assign new number to largest if greater than previous largest number
-				largestNumber = arr[i][x];
-			}
+		// Init counter as the first item in the array.
+		counter = arr[i][0];
+		// Loop for nested array.
+		for (let j = 1; j < arr[i].length; j++) {
+			// Check every item in array to see if it's greater than current counter.
+			arr[i][j] > counter ? (counter = arr[i][j]) : counter;
 		}
-		// Add largest number into final array
-		finalArray.push(largestNumber);
+		// Add the highest number to array only after the 2nd loop as finished.
+		largestArray.push(counter);
 	}
-	return finalArray;
-}
+	return largestArray;
+};
 
-largestOfFour([
-	[4, 5, 1, 3],
-	[13, 27, 18, 26],
-	[32, 35, 37, 39],
-	[1000, 1001, 857, 1]
-]);
+console.log(
+	largestOfFour([
+		[4, 5, 1, 3],
+		[13, 27, 18, 26],
+		[32, 35, 37, 39],
+		[1000, 1001, 857, 1]
+	])
+);
