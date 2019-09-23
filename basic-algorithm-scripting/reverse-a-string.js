c7605ca22aec6cdefc72d4ed5836738be15f20c7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-23 18:37:22 -0400

    Update solution

--- basic-algorithm-scripting/reverse-a-string.js
+++ basic-algorithm-scripting/reverse-a-string.js
@@ -4,11 +4,29 @@
 
 // Your result must be a string.
 
-function reverseString(str) {
-	return str
-		.split('')
-		.reverse()
-		.join('');
-}
+// function reverseString(str) {
+// 	return str
+// 		.split("")
+// 		.reverse()
+// 		.join("");
+// }
 
-reverseString('hello');
+const reverseString = str => {
+	return str
+		.split("")
+		.reverse()
+		.join("");
+};
+
+const reverseWithLoop = str => {
+	let reversedString = "";
+
+	// Loop through the string starting at the last index & create new string.
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversedString += str[i];
+	}
+	return reversedString;
+};
+
+console.log(reverseString("hello"));
+console.log(reverseWithLoop("cool"));
