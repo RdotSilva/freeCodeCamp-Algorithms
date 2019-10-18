4d35f3007decea7a11978163eb364b8a3541a6bb
Ryan Silva <RyandotSilva@gmail.com>
2019-10-18 10:45:15 -0400

    Add solution

---
+++ intermediate-algorithm-scripting/sorted-union.js
@@ -0,0 +1,25 @@
+// Write a function that takes two or more arrays and
+// returns a new array of unique values in the order of the original provided arrays.
+// In other words, all values present from all arrays should be included in their original order,
+// but with no duplicates in the final array.
+// The unique numbers should be sorted by their original order,
+// but the final array should not be sorted in numerical order.
+
+// Use regular function expression instead of arrow function
+// Arrow function does not have arguments binding.
+function uniteUnique(arr) {
+	let container = [];
+
+	let args = [...arguments];
+
+	for (let i = 0; i < args.length; i++) {
+		for (let j = 0; j < args[i].length; j++) {
+			if (!container.includes(args[i][j])) {
+				container.push(args[i][j]);
+			}
+		}
+	}
+	return container;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
